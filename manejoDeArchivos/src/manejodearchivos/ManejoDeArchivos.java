8c7f69fafbb50de291fd05e33b0ebb69692d87e7
luis mora <114058246+luuuisssmg2@users.noreply.github.com>
2022-10-11 10:09:01 +0200

    Carpeta Java

---
+++ manejoDeArchivos/src/manejodearchivos/ManejoDeArchivos.java
@@ -0,0 +1,23 @@
+/*
+ * Nuesta clase ManejoDeArchivos va a contener los siguientes metodos
+*- Crear Archivo
+*- Guardar la informacion de un Archivo
+*- Eliminar la informacion de un Archivo
+*- Eliminar Archivo
+ */
+package manejodearchivos;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class ManejoDeArchivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
