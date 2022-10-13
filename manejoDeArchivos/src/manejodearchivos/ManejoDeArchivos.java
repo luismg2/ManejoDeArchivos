b4d768769b278c16744fb6ee79808ce62e3cb4c8
luis mora <114058246+luuuisssmg2@users.noreply.github.com>
2022-10-13 14:11:17 +0200

    Dia2
    Segundo dia trabajando en este proyecto

--- manejoDeArchivos/src/manejodearchivos/ManejoDeArchivos.java
+++ manejoDeArchivos/src/manejodearchivos/ManejoDeArchivos.java
@@ -7,9 +7,16 @@
  */
 package manejodearchivos;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -30,6 +37,69 @@ public class ManejoDeArchivos {
         }catch(FileNotFoundException ex){
             ex.printStackTrace(System.out);
         }
+    }
+    public static void escribirArchivo(String nombre, String contenido){
+        PrintWriter salida=null;
+        File archivo=new File(nombre);
+        try {//Se que el fcihero existe
+            salida = new PrintWriter(archivo);
+            salida.println(contenido);
+            System.out.println("Se ha creado el archivo!!!");
+        } catch (FileNotFoundException ex) {//El fichero no existe => Excep.
+            ex.printStackTrace(System.out);
+        } finally {//Este bloque siempre se ejecuta
+            salida.close();
+        }
+        
+    }
+    public static void agregarArchivo(String nombre,String contenido){
+        File archivo=new File(nombre);
+        PrintWriter salida=null;
+        //PrintWriter salida=new PrintWriter(new FileWriter(nombre,true)); pero con el statement
+        try {
+            salida = new PrintWriter(new FileWriter(nombre,true));
+            salida.println(contenido);
+        } catch (IOException ex) {
+            
+        }finally{
+            salida.close();
+        }
+    }
+    public static void leerArchivo(String nombre){
+        File archivo=new File(nombre);
+        //BufferedReader entrada=new BufferedReader(new FileReader(archivo)); con el statement
+        try {
+            //Crear el descriptor de lectura del fichero
+            BufferedReader entrada=new BufferedReader(new FileReader(archivo));
+            String lectura=entrada.readLine();
+            //Recorremos lectura hasta final
+            while(lectura!=null){
+                System.out.println(lectura);
+                lectura=entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch(IOException ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+    public static void leerArchivoPorPalabra(String nombre){
+        File archivo =new File(nombre);
+        try {
+            Scanner entrada=new Scanner(archivo);
+            int cont=0;
+            //Recorro el fichero hasta fin
+            while(entrada.hasNext()){
+                String palabra=entrada.next();
+                System.out.println(palabra);
+                cont=cont+1;
+            }
+            System.out.println("Numero de palabras de mi fichero= "+cont);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
     }
     
 }
--- manejoDeArchivos/src/principal/PPal.java
+++ manejoDeArchivos/src/principal/PPal.java
@@ -14,6 +14,14 @@ import manejodearchivos.ManejoDeArchivos;
 public class PPal {
     public static void main(String[] args) {
         String nombreArchivo="prueba.txt";
+        String content="Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. \n"
+                + "Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500, cuando un impresor desconocido tomó una \n"
+                + "galera de tipos y la codificó para hacer un libro de muestras tipográficas. Ha sobrevivido no solo cinco siglos, sino también \n"
+                + "el salto a la composición tipográfica electrónica, permaneciendo esencialmente sin cambios.";
         ManejoDeArchivos.crearArchivo(nombreArchivo);
+        ManejoDeArchivos.escribirArchivo(nombreArchivo, content);
+        ManejoDeArchivos.agregarArchivo(nombreArchivo, "\nVizca Barca");
+        //ManejoDeArchivos.leerArchivo(nombreArchivo);
+        ManejoDeArchivos.leerArchivoPorPalabra(nombreArchivo);
     }
 }
